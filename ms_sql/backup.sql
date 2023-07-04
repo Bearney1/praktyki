d2d3271c2c4d5dcf5df7a6020776439875a12cfd
Marusiella <67511608+Marusiella@users.noreply.github.com>
2023-07-04 11:28:38 +0200

    add copy by powershell

---
+++ ms_sql/backup.sql
@@ -0,0 +1,2 @@
+BACKUP DATABASE [skip] TO  DISK = N'C:\Program Files\Microsoft SQL Server\MSSQL16.SQLEXPRESS\MSSQL\Backup\skip.bak' WITH NOFORMAT, NOINIT,  NAME = N'skip-Full Database Backup', SKIP, NOREWIND, NOUNLOAD,  STATS = 10
+GO
--- ms_sql/script.ps1
+++ ms_sql/script.ps1
@@ -1,14 +1,15 @@
-$sourceFile = "C:\Path\to\source\file.txt"
-$networkShare = "\\Server\Share"
-
-$destinationFileName = "file.txt"
-
+$sourceFile = "C:\Program Files\Microsoft SQL Server\MSSQL16.SQLEXPRESS\MSSQL\Backup\skip.bak"
+$networkShare = "\\192.168.1.123\Filmy Taty"
+$destinationFileName = "skip.back"
 $destinationPath = Join-Path -Path $networkShare -ChildPath $destinationFileName
 
 Copy-Item -Path $sourceFile -Destination $destinationPath -Force
 
 if (Test-Path -Path $destinationPath) {
     Write-Host "Successfully copied to $destinationPath"
+
+    Remove-Item -Path $sourceFile -Force
+    Write-Host "Source file deleted: $sourceFile"
 } else {
-    Write-Host "Failed to  $destinationPath"
+    Write-Host "Failed to copy to $destinationPath"
 }
