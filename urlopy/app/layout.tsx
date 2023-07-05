f0f5b7d3cff1a892b6adce4d627094c9bd036dbe
Marusiella <67511608+Marusiella@users.noreply.github.com>
2023-07-05 11:10:48 +0200

    prisma + auth

--- urlopy/app/layout.tsx
+++ urlopy/app/layout.tsx
@@ -1,3 +1,4 @@
+"use client"
 import Navbar from './Navbar'
 import './globals.css'
 import { Inter } from 'next/font/google'
--- urlopy/app/page.tsx
+++ urlopy/app/page.tsx
@@ -1,11 +1,17 @@
-"use client"
+// "use client"
 import Image from 'next/image'
 import { useSession, signIn, signOut } from "next-auth/react"
+import { Button } from '@/components/ui/button'
+import { getServerSession, unstable_getServerSession } from 'next-auth'
 
-export default function Home() {
+export default async function Home() {
+  const sesion = await getServerSession()
   return (
     <main className="flex  flex-col items-center justify-between p-24">
-
+      {/* <Button></Button> */}
+     <div className='text-white'>
+     {sesion?.user?.email}
+     </div>
     </main>
   )
 }
