b94d580764d50366efecc5ab622e6395aa653179
Marusiella <67511608+Marusiella@users.noreply.github.com>
2023-07-07 09:00:43 +0200

    add logging

--- ms_sql/script.ps1
+++ ms_sql/script.ps1
@@ -1,9 +1,9 @@
 $sourceDirectory = "C:\Program Files\Microsoft SQL Server\MSSQL16.SQLEXPRESS\MSSQL\Backup\"
 $networkShare = "\\192.168.1.123\Filmy Taty"
-$logFilePath = "\\192.168.1.123\Filmy Taty\backup_log.txt"
+$logFilePath = "\\192.168.1.123\Filmy Taty\Backups\backup_log.txt"
 
 $currentDate = Get-Date -Format "dd-MM-yyyy_hh-mm-ss"
-$destinationFolder = Join-Path -Path $networkShare -ChildPath "Backups/$currentDate"
+$destinationFolder = Join-Path -Path $networkShare -ChildPath "Backups\$currentDate"
 
 if (!(Test-Path -Path $destinationFolder)) {
     New-Item -Path $destinationFolder -ItemType Directory | Out-Null
@@ -12,6 +12,9 @@ if (!(Test-Path -Path $destinationFolder)) {
 
 $sourceFiles = Get-ChildItem -Path $sourceDirectory -Filter "*.bak" -File
 $filesInDestination = Get-ChildItem -Path $destinationFolder -Filter "*.bak" -File
+
+$logMessage = "Backuping files ($currentDate): `n"
+
 
 foreach ($sourceFile in $sourceFiles) {
     $destinationFileName = $sourceFile.Name -replace '\.bak$', "_$currentDate.bak"
@@ -24,6 +27,7 @@ foreach ($sourceFile in $sourceFiles) {
 
         Remove-Item -Path $sourceFile.FullName -Force
         Write-Host "Source file deleted: $($sourceFile.FullName)"
+        $logMessage += "File: $($sourceFile.FullName) - Sent to: $destinationPath`n"
     } else {
         Write-Host "Failed to copy to $destinationPath"
         return
@@ -35,3 +39,7 @@ if ($sourceFiles.Count -gt 0) {
         Remove-Item -Path $fileInDestination.FullName -Force
     }
 }
+
+$logMessage += "----------------------------------------`n"
+
+Add-Content -Path $logFilePath -Value $logMessage
